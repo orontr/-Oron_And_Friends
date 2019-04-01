7e3e68d9feb429584cbcc2167a382b51c9fbcd48
hadarco <hadarcoc@gmail.com>
2019-04-01 17:06:24 +0300

    doctor view

---
+++ AppointmentScheduling/AppointmentScheduling/ViewModel/AppointmentViewModel.cs
@@ -0,0 +1,15 @@
+﻿using AppointmentScheduling.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace AppointmentScheduling.ViewModel
+{
+    public class AppointmentViewModel
+    {
+        public Appointment Appointment { get; set; }
+        public List<Appointment> Appointments { get; set; }
+    }
+}
