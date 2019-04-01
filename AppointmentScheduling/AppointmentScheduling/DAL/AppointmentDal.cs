e9516db46812fbbb015617b1130748d9508f2f92
Oron Trabelsi <orontr@ac.sce.ac.il>
2019-04-01 17:06:06 +0300

    AppointmentScheduling of patient

---
+++ AppointmentScheduling/AppointmentScheduling/DAL/AppointmentDal.cs
@@ -0,0 +1,19 @@
+﻿using AppointmentScheduling.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AppointmentScheduling.DAL
+{
+    public class AppointmentDal:DbContext
+    {
+        public DbSet<Appointment> Appointments { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Appointment>().ToTable("Appointment");
+        }
+    }
+}
