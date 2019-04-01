4f4a85a270a093f26f776c44dd6e3ce4ad208508
hadarco <hadarcoc@gmail.com>
2019-04-01 12:52:28 +0300

    doctor
    model view
    controller
    doctor view - actions

--- AppointmentScheduling/AppointmentScheduling/Models/Doctor.cs
+++ AppointmentScheduling/AppointmentScheduling/Models/Doctor.cs
@@ -9,17 +9,26 @@ namespace AppointmentScheduling.Models
     public class Doctor {
         [Key]
         [Required]
-        public string DoctorLicenese { get; set; }
+        [RegularExpression("^[1-9]{5}$", ErrorMessage = "Doctor License must be 5 digits (digits 1-9)")]
+        public string DoctorLicense { get; set; }
+
         [Required]
         public string FirstName { get; set; }
+
         [Required]
         public string LastName { get; set; }
+
         [Required]
+        [RegularExpression("^[0-9]{9}$", ErrorMessage = "Doctor Id must be 9 digits (digits 0-9)")]
         public string DoctorID { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "The email address is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
         public string DoctorEmail { get; set; }
+
         [Required]
         public string DoctorSpecialization { get; set; }
+
         [Required]
         public string UserName { get; set; }
 
---
+++ AppointmentScheduling/AppointmentScheduling/ViewModel/DoctorViewModel.cs
@@ -0,0 +1,15 @@
+﻿using AppointmentScheduling.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace AppointmentScheduling.ViewModel
+{
+    public class DoctorViewModel
+    {
+        public Doctor Doctor { get; set; }
+        public List<Doctor> Doctors { get; set; }
+    }
+}
